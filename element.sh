f59071db7733641dc53d21f63b2b0b3dd16ce4e1
Inf3st4toR 1 <xcharaix@gmail.com>
2025-02-21 07:44:20 +0000

    feat: Improvement

--- element.sh
+++ element.sh
@@ -2,33 +2,30 @@
 #Setup
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 echo "Please provide an element as an argument."
+RESULT="I could not find that element in the database."
 
 #validate argument
 ARG=$1
-if [[ $ARG =~ ^[0-9]+$ ]]; then
 
-  # case number
+#case number
+if [[ $ARG =~ ^[0-9]+$ ]]; then
   ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN types USING(type_id) INNER JOIN elements USING(atomic_number) WHERE atomic_number = $ARG;")
   IFS="|" read EL_NB EL_NAME SYMB TYPE MASS MELT_P BOIL_P <<< $ELEMENT
   
-  #if not found
-  if [[ -z $ELEMENT ]]; then
-  RESULT="I could not find that element in the database."
-  else
-  RESULT="The element with atomic number $EL_NB is $EL_NAME ($SYMB). It's a $TYPE, with a mass of $MASS amu. $EL_NAME has a melting point of $MELT_P celsius and a boiling point of $BOIL_P celsius."
+  #if found
+  if [[ $ELEMENT ]]; then
+    RESULT="The element with atomic number $EL_NB is $EL_NAME ($SYMB). It's a $TYPE, with a mass of $MASS amu. $EL_NAME has a melting point of $MELT_P celsius and a boiling point of $BOIL_P celsius."
   fi
 
+#case string
 elif [[ $ARG =~ ^[a-zA-Z]+$ ]]; then
-
-  # case string
   ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN types USING(type_id) INNER JOIN elements USING(atomic_number) WHERE name = '$ARG' OR symbol = '$ARG';")
   IFS="|" read EL_NB EL_NAME SYMB TYPE MASS MELT_P BOIL_P <<< $ELEMENT
 
-  RESULT="The element with atomic number $EL_NB is $EL_NAME ($SYMB). It's a $TYPE, with a mass of $MASS amu. $EL_NAME has a melting point of $MELT_P celsius and a boiling point of $BOIL_P celsius."
-
-else
-  # case invalid
-  RESULT="I could not find that element in the database."
+  #if found
+  if [[ $ELEMENT ]]; then
+    RESULT="The element with atomic number $EL_NB is $EL_NAME ($SYMB). It's a $TYPE, with a mass of $MASS amu. $EL_NAME has a melting point of $MELT_P celsius and a boiling point of $BOIL_P celsius."
+  fi
 fi
 
 #output information
