996c19099002391dfe278928eec779ed908e3117
Inf3st4toR 1 <xcharaix@gmail.com>
2025-02-21 07:32:47 +0000

    feat: Validate Argument

--- element.sh
+++ element.sh
@@ -6,21 +6,25 @@ echo "Please provide an element as an argument."
 
 #validate argument
 ARG=$1
-echo "$ARG"
+if [[ $ARG =~ ^[0-9]+$ ]]; then
 
-#recover info in database
-# case number
-ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN types USING(type_id) INNER JOIN elements USING(atomic_number) WHERE atomic_number = $ARG;")
-IFS="|" read EL_NB EL_NAME SYMB TYPE MASS MELT_P BOIL_P <<< $ELEMENT
+  # case number
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN types USING(type_id) INNER JOIN elements USING(atomic_number) WHERE atomic_number = $ARG;")
+  IFS="|" read EL_NB EL_NAME SYMB TYPE MASS MELT_P BOIL_P <<< $ELEMENT
 
-echo "Int: $EL_NB $EL_NAME $SYMB $TYPE $MASS $MELT_P $BOIL_P"
+  echo "Int: $EL_NB $EL_NAME $SYMB $TYPE $MASS $MELT_P $BOIL_P"
 
-# case string
-ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN types USING(type_id) INNER JOIN elements USING(atomic_number) WHERE name = '$ARG' OR symbol = '$ARG';")
-IFS="|" read EL_NB EL_NAME SYMB TYPE MASS MELT_P BOIL_P <<< $ELEMENT
+elif [[ $ARG =~ ^[a-zA-Z]+$ ]]; then
 
-echo "String: $EL_NB $EL_NAME $SYMB $TYPE $MASS $MELT_P $BOIL_P"
+  # case string
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN types USING(type_id) INNER JOIN elements USING(atomic_number) WHERE name = '$ARG' OR symbol = '$ARG';")
+  IFS="|" read EL_NB EL_NAME SYMB TYPE MASS MELT_P BOIL_P <<< $ELEMENT
 
+  echo "String: $EL_NB $EL_NAME $SYMB $TYPE $MASS $MELT_P $BOIL_P"
+
+else
+  echo "I could not find that element in the database."
+fi
 
 #output information
 
