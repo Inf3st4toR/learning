01ce38585a04176b6f6920b565761b3f3960b845
Inf3st4toR 1 <xcharaix@gmail.com>
2025-02-21 06:42:59 +0000

    feat: Initial Setup Done

---
+++ element.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+echo "Please provide an element as an argument."
+
+#read argument
+
+#validate argument
+
+#recover info in database
+
+#output information
