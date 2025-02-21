3afe2819833f9f1d34b333a8dd485c5da2e1dcc2
Inf3st4toR 1 <xcharaix@gmail.com>
2025-02-21 07:59:50 +0000

    fix: fix last issue

--- element.sh
+++ element.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Setup
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+RESULT="I could not find that element in the database."
 
 #case empty
 if [[ -z $1 ]]; then
@@ -11,7 +12,7 @@ else
   if [[ $1 =~ ^[0-9]+$ ]]; then
     ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN types USING(type_id) INNER JOIN elements USING(atomic_number) WHERE atomic_number = $1;")
     IFS="|" read EL_NB EL_NAME SYMB TYPE MASS MELT_P BOIL_P <<< $ELEMENT
-    
+  
     #if found
     if [[ $ELEMENT ]]; then
       RESULT="The element with atomic number $EL_NB is $EL_NAME ($SYMB). It's a $TYPE, with a mass of $MASS amu. $EL_NAME has a melting point of $MELT_P celsius and a boiling point of $BOIL_P celsius."
@@ -26,12 +27,8 @@ else
     if [[ $ELEMENT ]]; then
       RESULT="The element with atomic number $EL_NB is $EL_NAME ($SYMB). It's a $TYPE, with a mass of $MASS amu. $EL_NAME has a melting point of $MELT_P celsius and a boiling point of $BOIL_P celsius."
     fi
-
-  #if not found
-  else
-    RESULT="I could not find that element in the database."
   fi
 fi
 #output information
-echo -e "\n$RESULT"
+echo -e "$RESULT"
 
