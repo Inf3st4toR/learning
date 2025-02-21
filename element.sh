1d8d6516041a9709743efbd9aab0e9fc4b8cba6c
Inf3st4toR 1 <xcharaix@gmail.com>
2025-02-21 07:54:43 +0000

    fix: Fixed all bugs

--- element.sh
+++ element.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 #Setup
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
-echo "Please provide an element as an argument."
-RESULT="I could not find that element in the database."
 
-#validate argument
-ARG=$1
+#case empty
+if [[ -z $1 ]]; then
+  RESULT="Please provide an element as an argument."
 
-#case number
-if [[ $ARG =~ ^[0-9]+$ ]]; then
-  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN types USING(type_id) INNER JOIN elements USING(atomic_number) WHERE atomic_number = $ARG;")
-  IFS="|" read EL_NB EL_NAME SYMB TYPE MASS MELT_P BOIL_P <<< $ELEMENT
-  
-  #if found
-  if [[ $ELEMENT ]]; then
-    RESULT="The element with atomic number $EL_NB is $EL_NAME ($SYMB). It's a $TYPE, with a mass of $MASS amu. $EL_NAME has a melting point of $MELT_P celsius and a boiling point of $BOIL_P celsius."
-  fi
+else
+  #case number
+  if [[ $1 =~ ^[0-9]+$ ]]; then
+    ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN types USING(type_id) INNER JOIN elements USING(atomic_number) WHERE atomic_number = $1;")
+    IFS="|" read EL_NB EL_NAME SYMB TYPE MASS MELT_P BOIL_P <<< $ELEMENT
+    
+    #if found
+    if [[ $ELEMENT ]]; then
+      RESULT="The element with atomic number $EL_NB is $EL_NAME ($SYMB). It's a $TYPE, with a mass of $MASS amu. $EL_NAME has a melting point of $MELT_P celsius and a boiling point of $BOIL_P celsius."
+    fi
 
-#case string
-elif [[ $ARG =~ ^[a-zA-Z]+$ ]]; then
-  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN types USING(type_id) INNER JOIN elements USING(atomic_number) WHERE name = '$ARG' OR symbol = '$ARG';")
-  IFS="|" read EL_NB EL_NAME SYMB TYPE MASS MELT_P BOIL_P <<< $ELEMENT
+  #case string
+  elif [[ $1 =~ ^[a-zA-Z]+$ ]]; then
+    ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN types USING(type_id) INNER JOIN elements USING(atomic_number) WHERE name = '$1' OR symbol = '$1';")
+    IFS="|" read EL_NB EL_NAME SYMB TYPE MASS MELT_P BOIL_P <<< $ELEMENT
 
-  #if found
-  if [[ $ELEMENT ]]; then
-    RESULT="The element with atomic number $EL_NB is $EL_NAME ($SYMB). It's a $TYPE, with a mass of $MASS amu. $EL_NAME has a melting point of $MELT_P celsius and a boiling point of $BOIL_P celsius."
+    #if found
+    if [[ $ELEMENT ]]; then
+      RESULT="The element with atomic number $EL_NB is $EL_NAME ($SYMB). It's a $TYPE, with a mass of $MASS amu. $EL_NAME has a melting point of $MELT_P celsius and a boiling point of $BOIL_P celsius."
+    fi
+
+  #if not found
+  else
+    RESULT="I could not find that element in the database."
   fi
 fi
-
 #output information
 echo -e "\n$RESULT"
 
