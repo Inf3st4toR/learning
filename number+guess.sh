a2d7b803d21aa0bf3c053d34c926194587a015f5
Inf3st4toR 1 <xcharaix@gmail.com>
2025-03-11 04:28:39 +0000

    Initial commit

---
+++ number+guess.sh
@@ -0,0 +1,21 @@
+`#!/bin/bash'
+
+`PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"`
+
+#Validate username
+read -p "Enter your username:" USERNAME
+RESULT=$($PSQL "SELECT username FROM users WHERE name='$USERNAME'")
+if [[ -z $RESULT ]]; then
+    #Not found
+    echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+else
+    #Found
+    echo -e "\nWelcome back, $USERNAME! ..."
+fi
+
+#Guess number
+RAN_NUM=$((($RANDOM % 1000)+1))
+echo -e "\nGuess the secret number between 1 and 1000:"
+read GUESS
+
+echo $GUESS
